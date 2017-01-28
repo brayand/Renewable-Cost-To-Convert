848aabfd88ac900525042fe6868260e424b39018
brayand <bray.andrew.0@gmail.com>
2017-01-28 15:03:53 -0600

    Check that api key doesn't show up on GitHub

--- Total Cost to Convert to Renewables.R
+++ Total Cost to Convert to Renewables.R
@@ -1,17 +1,23 @@
 #####################################
-#
-#
-#
+#Total Cost to Convert the Electric to Renewable Energy in 3 Steps
+#1-28-16
+#Andrew Bray
 #####################
 
 
 
 #Get API Data
+eia_api_key = readLines('eia.api.key.config')
+
 
 #Put into Database with SQL
 
-##Step 1 - Simple
+##Step 1 - Fossil Fuel Output and LCOE of Renewables
+#MWh of Energy per Year of Fossil Fuel
+#Average LCOE Cost of Renewables
 
-##Step 2
 
-##Step 3
+
+##Step 2 - Add TOU data
+
+##Step 3 - Add location Data
